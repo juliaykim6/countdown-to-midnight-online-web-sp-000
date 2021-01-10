bab2b80c66ebc5f7d193714c69f9e7b59c11a4f3
Julia Kim <juliaykim6@gmail.com>
2021-01-10 21:38:39 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
 #write your code here
 def countdown (number)
   number = 0
-  while counter > 1 
+  while counter > 1
     puts "#{number} SECOND(S)!"
-    counter -= 1 
+    counter -= 1
     
 end
