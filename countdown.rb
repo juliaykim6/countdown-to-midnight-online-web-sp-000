5f25af8fb1b851e74a08ea99d3743c8037eeb25d
Julia Kim <juliaykim6@gmail.com>
2021-01-10 21:42:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,8 @@
 #write your code here
 def countdown (number)
-  number = 0
-  while number > 1
+  while number > 0
     puts "#{number} SECOND(S)!"
-    counter -= 1
+    number -= 1
   end
   return "HAPPY NEW YEAR!"
 end
